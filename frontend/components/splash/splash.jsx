833c2f8fddb3dd8a8da1d0d64b47202c5188b960
Donnie <donald.batryn@gmail.com>
2019-11-22 16:14:14 -0600

    animations on all splash slides

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -14,6 +14,26 @@ const Slide2Box2 = posed.div({
     hidden: { right: -1500, bottom: 230 }
 })
 
+const Slide3Box1 = posed.div({
+    visible: { opacity: 1, transition: { duration: 500 } },
+    hidden: { opacity: 0, transition: { duration: 500 } }
+})
+
+const Slide3Box2 = posed.div({
+    visible: { right: 10, transition: { duration: 300 } },
+    hidden: { right: -1500  }
+})
+
+const Slide4Box1 = posed.div({
+    visible: { opacity: 1, transition: { duration: 500 } },
+    hidden: { opacity: 0, transition: { duration: 500 } }
+})
+
+const Slide4Box2 = posed.div({
+    visible: { opacity: 1, transition: { duration: 500, delay: 900 } },
+    hidden: { opacity: 0, transition: { duration: 500 } }
+})
+
 class Splash extends React.Component {
     constructor(props) {
         super(props);
@@ -23,7 +43,9 @@ class Splash extends React.Component {
             splashClass3: "",
             splashClass4: "",
             splashClass5: "",
-            slide2isVisible: false
+            slide2isVisible: false,
+            slide3isVisible: false,
+            slide4isVisible: false
         }
         this.handleWheel1 = this.handleWheel1.bind(this);
         this.handleWheel2 = this.handleWheel2.bind(this);
@@ -41,39 +63,45 @@ class Splash extends React.Component {
             setTimeout(() => {
                 this.setState({ slide2isVisible: true })
             }, 700)
-            
         }
-        
     }
 
     handleWheel2(e) {
         e.stopPropagation();
         if (e.deltaY < 0) {
             this.setState({ splashClass1: 'scroll-down', slide2isVisible: false  });
-            console.log('trig')
         } else {
             this.setState({ splashClass2: 'scroll-two', slide2isVisible: false });
+            setTimeout(() => {
+                this.setState({ slide3isVisible: true })
+            }, 700)
         }
     }
 
     handleWheel3(e) {
         e.stopPropagation();
         if (e.deltaY < 0) {
-            this.setState({ splashClass2: 'scroll-down'  });
+            this.setState({ splashClass2: 'scroll-down', slide3isVisible: false  });
             setTimeout(() => {
                 this.setState({ slide2isVisible: true })
             }, 700)
         } else {
-            this.setState({ splashClass3: 'scroll-three', slide2isVisible: true });
+            this.setState({ splashClass3: 'scroll-three', slide3isVisible: false });
+            setTimeout(() => {
+                this.setState({ slide4isVisible: true })
+            }, 700)
         }
     }
 
     handleWheel4(e) {
         e.stopPropagation();
         if (e.deltaY < 0) {
-            this.setState({ splashClass3: 'scroll-down', slide2isVisible: false  });
+            this.setState({ splashClass3: 'scroll-down', slide4isVisible: false  });
+            setTimeout(() => {
+                this.setState({ slide3isVisible: true })
+            }, 700)
         } else {
-            this.setState({ splashClass4: 'scroll-four' });
+            this.setState({ splashClass4: 'scroll-four', slide4isVisible: false });
         }
     }
 
@@ -81,6 +109,9 @@ class Splash extends React.Component {
         e.stopPropagation();
         if (e.deltaY < 0) {
             this.setState({ splashClass4: 'scroll-down', slide2isVisible: false  });
+            setTimeout(() => {
+                this.setState({ slide4isVisible: true })
+            }, 700)
         } else {
             this.setState({ splashClass5: 'scroll-five' });
         }
@@ -112,10 +143,24 @@ class Splash extends React.Component {
                     </Slide2Box2 >
                 </div>
                 <div className={`splash-3 ${this.state.splashClass3}`} onWheel={this.handleWheel3}>
-
+                    <Slide3Box1 className="splash3-box1" pose={this.state.slide3isVisible ? 'visible' : 'hidden'}>
+                        <img className="spl-img" src={window.textButton} alt=""/>
+                        <img className="spl-img" src={window.photoButton} alt=""/>
+                        <img className="spl-img" src={window.quoteButton} alt=""/>
+                    </Slide3Box1>
+                    <Slide3Box2 className="splash3-box2" pose={this.state.slide3isVisible ? 'visible' : 'hidden'}>
+                        <h3 className="splash3-tumbly-subheader">Write some text...</h3>
+                        <h3 className="splash3-tumbly-subheader">Post a photo...</h3>
+                        <h3 className="splash3-tumbly-subheader">or quote someone cool.</h3>
+                    </Slide3Box2>
                 </div>
                 <div className={`splash-4 ${this.state.splashClass4}`} onWheel={this.handleWheel4}>
-
+                    <Slide4Box1 className="splash4-box1" pose={this.state.slide4isVisible ? 'visible' : 'hidden'}>
+                        <h1>Your Tumbly.</h1>
+                    </Slide4Box1>
+                    <Slide4Box2 className="splash4-box2" pose={this.state.slide4isVisible ? 'visible' : 'hidden'}>
+                        <h1>Your world.</h1>
+                    </Slide4Box2>
                 </div>
                 <div className={`splash-5 ${this.state.splashClass5}`} onWheel={this.handleWheel5}>
                     {sessionForm}
