d41957ab6dc9007b7c8860f181230430b7c1c275
Donnie <donald.batryn@gmail.com>
2019-11-27 17:55:27 -0600

    made coming soon message

--- frontend/components/nav/navbar.jsx
+++ frontend/components/nav/navbar.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import Search from '../search/search';
 import { Link } from 'react-router-dom'
+import posed from 'react-pose';
+
+const ComingSoon = posed.div({
+    visible: { opacity: 1, transition: { duration: 500 } },
+    hidden: { opacity: 0, transition: { duration: 500 } }
+})
 
 
 class NavBar extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            comingSoonisVisible: false
+        }
         this.logoutUser = this.logoutUser.bind(this);
         this.getLinks = this.getLinks.bind(this);
+        this.handleMsg - this.handleMsg.bind(this);
+    }
+
+    handleMsg(){
+        let comingSoon = document.getElementsByClassName('coming-soon-msg')[0]
+        let styles = window.getComputedStyle(comingSoon);
+        if (styles.display === 'none') comingSoon.style.display = 'flex';
+        this.setState({ comingSoonisVisible: true })
+        setTimeout(() => {
+            this.setState({ comingSoonisVisible: false })
+            comingSoon.style.display = 'none';
+        }, 2200)
     }
 
     logoutUser(e) {
@@ -28,11 +49,20 @@ class NavBar extends React.Component {
                     </div>
                     <div className="navbar-center">
                         <Link className="nav-icon" to="/dashboard"><i className="myicon fa fa-home"></i></Link>
-                        <Link className="nav-icon" to="/dashboard"><i className="myicon fa fa-compass"></i></Link>
-                        <Link className="nav-icon" to="/dashboard"><i className="myicon fa fa-envelope"></i></Link>
-                        <Link className="nav-icon" to="/dashboard"><i className="myicon fa fa-comment"></i></Link>
-                        <Link className="nav-icon" to="/dashboard"><i className="myicon fa fa-bolt"></i></Link>
+                        <button className="nav-icon" onClick={() => this.handleMsg()}><i className="myicon fa fa-compass"></i></button>
+                        <button className="nav-icon" onClick={() => this.handleMsg()}><i className="myicon fa fa-envelope"></i></button>
+                        <button className="nav-icon" onClick={() => this.handleMsg()}><i className="myicon fa fa-comment"></i></button>
+                        <button className="nav-icon" onClick={() => this.handleMsg()}><i className="myicon fa fa-bolt"></i></button>
                         <button className="nav-icon" onClick={() => this.props.openModal('Profile')}><i className="myicon fa fa-user"></i></button>
+                        {/* <ComingSoon className="coming-soon-msg" pose={this.state.comingSoonisVisible ? 'visible' : 'hidden'}>
+                            <h1 className="coming-soon-txt">Feature coming soon!</h1>
+                        </ComingSoon> */}
+                        <ComingSoon className="coming-soon-msg" pose={this.state.comingSoonisVisible ? 'visible' : 'hidden'}>
+                            <div className="text-box-comingsoon">
+                            <h1 className="coming-soon-txt">Feature coming soon!</h1>
+
+                            </div>
+                        </ComingSoon>
                     </div>
                     <div className="navbar-right">
                         <button className="logout-button" onClick={this.logoutUser}>Logout</button>
